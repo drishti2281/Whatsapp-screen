27d5375e2a2dfb608d0e395d391311379ca0e25b
drishti2281 <121477988+drishti2281@users.noreply.github.com>
2024-02-27 23:50:27 +0530

    check description

--- app/src/main/java/com/example/whatsappscreen/MainActivity.kt
+++ app/src/main/java/com/example/whatsappscreen/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.whatsappscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.whatsappscreen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var layout = arrayListOf(R.layout.fragment_chat,
+        R.layout.fragment_status,
+        R.layout.fragment_call)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
